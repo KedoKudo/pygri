939d0c029ae0d868f6399e2d6be60cb2875f740d
Chen <chenzhang8722@gmail.com>
2017-10-03 09:19:12 -0400

    blank template for migrating Gri to matplotlib
    it takes way too much time to migrate one package over, need to put it on hold until finishing the strain refinement paper.

--- cdf.py
+++ cdf.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 from pygri.pltmath import calc_cummulative_dist
 
 
-def cdf(data, 
-        labels=None, labelsize='small', 
-        figureSize=(3,3), 
+def cdf(data,
+        labels=None, labelsize='small',
+        figureSize=(3,3),
         cmap="Blues", steps=100,
         xlim=None, ylim=None,
         xscale="linear", yscale="linear",
@@ -24,21 +24,21 @@ def cdf(data,
         labels:     strs | list of strs
             Labels for each set of data. If DataFrame is passed, it is also
             used to quert the data column from given DataFrame.
-        labelsize:  label size contorl 
-            Valid input inlcudes: 
-            large, medium, smaller, small, x-large, xx-small, 
+        labelsize:  label size contorl
+            Valid input inlcudes:
+            large, medium, smaller, small, x-large, xx-small,
             larger, x-small, xx-large, None
         figureSize: tuple
             Figure size in inches.
         cmap:       Matplotlib built-in cmap names
-            If multiple data are passed, this cmap is used to map the variation 
+            If multiple data are passed, this cmap is used to map the variation
             among given datas.
         steps:      Int
             Number of data points plotted on figure.
         x/yscale:   specify axis types
             valid options = [“linear”, “log”, “symlog”, “logit”]
     returns:
-        fig:        Matplotlib figure instance 
+        fig:        Matplotlib figure instance
             A figure instance is returned for futher tweaking if necessary
         ax:         Matplotlib axes handle
             Allow further tweaking outside the function
@@ -54,7 +54,7 @@ def cdf(data,
         except:
             column_nums = 1
         labels = ["data_{}".format(i+1) for i in xrange(column_nums)]
-    
+
     # setting up figure
     fig = plt.figure(figsize=figureSize, dpi=300)
     ax = fig.add_subplot(1, 1, 1)
@@ -63,13 +63,13 @@ def cdf(data,
     if len(labels) > 1:
         cmap = plt.get_cmap(cmap)
         clrs = cmap(np.linspace(0.1, 0.9, len(labels)))
-    
+
     for i, label in enumerate(labels):
         _pltx, _plty = calc_cummulative_dist(pltdata[:, i], steps=steps)
         ax.plot(_pltx, _plty, color=clrs[i], label=label)
-    
+
     # formatting ticks to mimic GRI output
-    ax.tick_params(direction='in', 
+    ax.tick_params(direction='in',
                    length=3, width=0.2,
                    labelsize=labelsize, pad=1,
                    which='both',
@@ -86,6 +86,7 @@ def cdf(data,
         ax.set_ylim(ylim)
 
     return fig, ax
+
 
 if __name__ == "__main__":
     testData = np.random.random((1000, 3))
---
+++ plot.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
+
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
+def plot(dataframe, xcolumns, ycolumns,
+         labels=None,
+         linestyles=None,
+         linewidth=1.0,
+         cmap="Blues",
+         ):
+    pass
+
+
+if __name__ == "__main__":
+    print("GRI style plot demo")

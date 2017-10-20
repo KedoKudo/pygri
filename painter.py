1bf1111718ad0603e7f6f0205ec2b4f32dbbf3c6
Chen <chenzhang8722@gmail.com>
2017-10-20 14:33:42 -0400

    fix error in CDF calculation

---
+++ painter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
+
+
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import macplotlib.pyplot as plt
+
+
+def painter(tasks, 
+            fig_size=(3,3), 
+            ):
+    """
+    carry out the plotting tasks specified in the task list
+    
+    parameters:
+
+    returns:
+    """
+    pass
+
+
+if __name__ == "__main__":
+    print("Testing painter function")
--- pltmath.py
+++ pltmath.py
@@ -9,9 +9,9 @@ import pandas as pd
 def calc_cummulative_dist(data, label=None, steps=None):
     """
     description:
-        Calculated the cumulative distribution from a data array without using 
+        Calculated the cumulative distribution from a data array without using
         binning.  This provides a more robust way to representing the statistics
-        of the incoming data without worrying about the binning/samping effect.   
+        of the incoming data without worrying about the binning/samping effect.
     args:
         data:       1D np.array | full pd.DataFrame
             1-D numpy array or dataFrame (when label is provided)
@@ -29,19 +29,19 @@ def calc_cummulative_dist(data, label=None, steps=None):
         x = np.sort(data[label])
     else:
         x = np.sort(data)
-    
+
     # calculate the cumulative density
     xx = np.tile(x, (2, 1)).flatten(order='F')
-    y = np.cumsum(range(len(x)))
+    y = np.arange(len(x))
     yy = np.vstack((y, y+1)).flatten(order='F')/float(y[-1])
 
-    # subsamping if steps is speficiied and the number is smaller than the 
-    # total lenght of x 
+    # subsamping if steps is speficiied and the number is smaller than the
+    # total lenght of x
     if (steps is not None) and len(x) > steps:
         idx = np.arange(0, len(x), int(np.ceil(len(x)/steps)))
         xx = xx[idx]
         yy = yy[idx]
-    
+
     return xx, yy
 
 
